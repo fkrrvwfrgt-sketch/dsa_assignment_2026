f0d47494fce83b865aafdcf650fd70c84b606fbd
RaRa <fkrrvwfrgt@privaterelay.appleid.com>
2026-02-10 18:05:29 +0545

    Refactor and clean up linked list implementation

--- prog_3.c
+++ prog_3.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node structure
 struct Node {
     int data;
     struct Node* next;
 };
 
-// Function to insert at the beginning
 void push(struct Node** head_ref, int new_data) {
     struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
     new_node->data = new_data;
@@ -15,19 +13,15 @@ void push(struct Node** head_ref, int new_data) {
     (*head_ref) = new_node;
 }
 
-// Task 3: Reverse Traversal Function (Recursive)
 void reverseTraversal(struct Node* head) {
     if (head == NULL)
         return;
 
-    // First recur on the rest of the list
+  
     reverseTraversal(head->next);
-
-    // Then print the data
     printf("%d ", head->data);
 }
 
-// Function to print the list normally
 void printList(struct Node* head) {
     while (head != NULL) {
         printf("%d -> ", head->data);
@@ -38,8 +32,6 @@ void printList(struct Node* head) {
 
 int main() {
     struct Node* head = NULL;
-
-    // Creating a list: 10 -> 20 -> 30 -> 40
     push(&head, 40);
     push(&head, 30);
     push(&head, 20);
@@ -53,4 +45,5 @@ int main() {
     printf("\n");
 
     return 0;
+
 }
