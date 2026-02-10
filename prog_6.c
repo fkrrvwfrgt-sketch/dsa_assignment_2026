b11d0de3bd9dd2a613430cbb8ad1f05ccea1547c
RaRa <fkrrvwfrgt@privaterelay.appleid.com>
2026-02-10 17:46:57 +0545

    Add files via upload

---
+++ prog_6.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+int size = 0;
+
+void swap(int *a, int *b) {
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
+
+// Maintains the heap property after insertion or deletion
+void heapify(int array[], int size, int i) {
+    if (size == 1) return;
+
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    // For Min-Heap, change > to <
+    if (l < size && array[l] > array[largest])
+        largest = l;
+    if (r < size && array[r] > array[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(&array[i], &array[largest]);
+        heapify(array, size, largest);
+    }
+}
+
+void insert(int array[], int newNum) {
+    if (size == 0) {
+        array[0] = newNum;
+        size += 1;
+    } else {
+        array[size] = newNum;
+        size += 1;
+        // Heapify up
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(array, size, i);
+        }
+    }
+}
+
+void deleteRoot(int array[], int num) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (num == array[i]) break;
+    }
+
+    swap(&array[i], &array[size - 1]);
+    size -= 1;
+    for (int i = size / 2 - 1; i >= 0; i--) {
+        heapify(array, size, i);
+    }
+}
+
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; ++i)
+        printf("%d ", array[i]);
+    printf("\n");
+}
+
+int main() {
+    int array[10];
+
+    insert(array, 3);
+    insert(array, 9);
+    insert(array, 2);
+    insert(array, 1);
+    insert(array, 5);
+
+    printf("Max-Heap array: ");
+    printArray(array, size);
+
+    deleteRoot(array, 9);
+    printf("After deleting element 9: ");
+    printArray(array, size);
+
+    return 0;
+}
